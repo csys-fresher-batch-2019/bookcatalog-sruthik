5af1fbbce78681a8a605b2209a4bfcf7bbe5e634
SRUTHI K <sruthik2020@srishakthi.ac.in>
2020-01-30 14:54:43 +0530

    Create DisplayByCourseCodeAPI.java

---
+++ src/DisplayByCourseCodeAPI.java
@@ -0,0 +1,33 @@
+package com.sruthi.TestTitles;
+
+import java.util.List;
+import java.util.Scanner;
+
+import com.sruthi.DAOFactory;
+import com.sruthi.Logger;
+import com.sruthi.dao.TitleDAO;
+import com.sruthi.impl.Title;
+
+public class DisplayForCouseId {
+	private static final Logger LOGGER = Logger.getInstance();
+	public static void main(String[] args)  {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter Course-Code : ");
+		int id = in.nextInt();
+		System.out.println("Recommended Books...");
+        TitleDAO dao = DAOFactory.getTitleDAO();
+        try {
+			List<Title> list = dao.displayTitleForCourseId(id);
+			for (Title title : list) {
+				LOGGER.debug("Title-id : "+title.getTitleId()+"\nPublisher-id : "+title.getPubId()+"\nAuthor-id : "+title.getAuthorId()+"\ntitle: "+title.getTitle()+"\nprice: "+title.getPrice()+"\nversion: "+title.getVersionNumber());
+
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			LOGGER.debug("No Contents to display");
+		}
+        in.close();
+	}
+
+}
