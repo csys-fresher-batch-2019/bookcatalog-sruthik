82da5025212e32926255423936e3dc21821be534
Sruthi K <sruthi.karuppasamy@global.chainsys.com>
2020-02-17 10:57:04 +0530

    updated

--- src/main/java/com/sruthi/ConnectionUtil.java
+++ src/main/java/com/sruthi/ConnectionUtil.java
@@ -4,8 +4,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import org.jdbi.v3.core.Jdbi;
-import org.jdbi.v3.sqlobject.SqlObjectPlugin;
 
 public class ConnectionUtil {
 	private ConnectionUtil() {
@@ -28,16 +26,5 @@ public class ConnectionUtil {
 	public static void main(String[] args) throws SQLException {
 		System.out.println(getConnection());
 	}
-	public static Jdbi getJdbi() {
-		Connection connection = null;
-		try {
-			connection = ConnectionUtil.getConnection();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		Jdbi jdbi = Jdbi.create(connection);
-		jdbi.installPlugin(new SqlObjectPlugin());
-		return jdbi;
-	}
+	
 }
